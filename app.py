581dfaafe165b0d539ae9b2aaf0abd4610186fb0
ikhwanzubir <52627770+ikhwanzubir@users.noreply.github.com>
2023-10-17 04:12:45 +0000

    Added cangain and needtolose

--- app.py
+++ app.py
@@ -16,11 +16,16 @@ def calculate():
 
 # Calculate the weight for the target BMI using the formula: weight = BMI * height^2
     target_weight = target_bmi * (height_in_meters**2)
-    difference = round(target_weight - weight, 2)
+
+    if target_weight > weight:
+      needtolose = round(target_weight - weight, 2)
+
+    if weight > target_weight:
+      cangain = round(weight - target_weight, 2)
 
     return render_template('bmi_calculator.html', height=height_in_meters, 
                            weight=weight, bmi=bmi, target_bmi=target_bmi, 
-                           difference=difference)
+                           needtolose=needtolose, cangain=cangain)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
