11dfa8edd310df54c13cb5c431fbee448b68a508
ikhwanzubir <52627770+ikhwanzubir@users.noreply.github.com>
2023-10-17 03:24:04 +0000

    Edited to include target bmi and differences in kg

--- app.py
+++ app.py
@@ -8,10 +8,17 @@ def index():
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
-    height = float(request.form.get('height')) / 100  # convert cm to meters
+    height_in_meters = float(request.form.get('height')) / 100  # convert cm to meters
     weight = float(request.form.get('weight'))
-    bmi = round(weight / (height**2), 2)
-    return render_template('bmi_calculator.html', bmi=bmi)
+    target_bmi = float(request.form.get('target_bmi'))
+
+    bmi = round(weight / (height_in_meters**2), 2)
+
+    # Calculate the weight for the target BMI using the formula: weight = BMI * height^2
+    target_weight = target_bmi * (height_in_meters**2)
+    difference = round(target_weight - weight, 2)
+
+    return render_template('bmi_calculator.html', bmi=bmi, target_bmi=target_bmi, difference=difference)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
